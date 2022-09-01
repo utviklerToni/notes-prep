f7c6ba02d051d56212ec5837a0f25d897c12d30b
utviklerToni <h1de.toni@protonmail.com>
2022-09-01 03:49:50 +0200

    typescript

---
+++ typescript/basics.ts
@@ -0,0 +1,20 @@
+How to proceed to master TS 
+
+syntax+features
+    what is an interface?
+    
+    what is the syntax for defining an interface?
+
+desgin patterns with TS
+    how do we use interfaces to write reusable code?
+    
+    
+    goals
+        understand basic types in ts
+        function typing + annotations
+        type definition files
+        arrays in TS
+        modules systems
+        classes + Refresher on OOP
+        
+        Projects
